fa9c2186154261ac92412c0c75ec0addb4f4e335
Alex B <alex.blackburn1995@gmail.com>
2023-02-06 18:17:18 -0500

    setting up Agora

--- src/components/HelpWidget.tsx
+++ src/components/HelpWidget.tsx
@@ -1,5 +1,5 @@
-import AgoraRTM from "agora-rtm-sdk";
-import { useEffect, useState } from "react"
+import AgoraRTM, { RtmChannel } from "agora-rtm-sdk";
+import { useEffect, useRef, useState } from "react"
 import { api } from "../utils/api"
 
 
@@ -12,6 +12,8 @@ type Tmessage ={
 export const HelpWidget = () => {
     const createHelpRequestMutation = api.helpRequest.createHelpRequest.useMutation()
 
+
+    const [ text, setText ] = useState('')
     const [ isChatPanelDisplayed, setIsChatPanelDisplayed ] = useState(false)
     const [ senderId, setSenderId ] = useState("0")
     const [ messages, setMessages ] = useState<Tmessage[]>([
@@ -32,12 +34,43 @@ export const HelpWidget = () => {
         sender: "0" 
     },
     ]);
+    const channelRef = useRef<RtmChannel | null>(null);
 
     const handleOpenSupportWidget = async () => {
         setIsChatPanelDisplayed(true)
         const helpRequest = await createHelpRequestMutation.mutateAsync();
         const { default: AgoraRTM } = await import("agora-rtm-sdk")
         const client = AgoraRTM.createInstance(process.env.NEXT_PUBLIC_AGORA_ID!)
+        await client.login({
+            uid: `${Math.floor(Math.random() * 300)}`,
+            token: undefined
+        })
+        const channel = await client.createChannel(helpRequest.id)
+        await channel.join()
+        channel.on('ChannelMessage', (message) => {
+            if(!message.text) return;
+            setMessages((prevMessages) => [
+                ...prevMessages,
+                { 
+                message: message.text ?? "",
+                id: Math.random() + "",
+                sender: "1"
+                },
+            ]);
+        });
+    };
+
+    const handleSendMessage = async () => {
+     const channel = channelRef.current;
+        channel?.sendMessage({ text })
+        setMessages((prevMessages) => [
+            ...prevMessages,
+            {
+                message: text,
+                id: Math.random() + "",
+                sender: "0"
+            },
+        ]);
     }
 
    
@@ -49,18 +82,18 @@ export const HelpWidget = () => {
             <button className="absolute  top-4 right-4 hover:text-red-500d"
             onClick={() => setIsChatPanelDisplayed(false)}
             >X</button>
-            <div>
+            
                 <ul>
                     {messages.map(({message, id, sender}) => (
                         <li
-                        className={` rounded p-1 mb-2 ${sender === senderId ? 'bg-gray-100' : 'bg-blue-300'}`}
+                        className={`rounded p-1 mb-2 ${sender === senderId ? 'bg-gray-100' : 'bg-blue-300'}`}
                         key={id}>{message}</li>
                     
                     ))}
                 </ul>
-            </div>
-            <form className="flex ">
-            <input type="text" className="border border-gray-800 rounded h-17 p-1 px-2" ></input>
+        
+            <form onChange={e => setText(e.target.value)} onSubmit={handleSendMessage} className="flex ">
+            <input value={text} className="border border-gray-800 rounded h-17 p-1 px-2" ></input>
             <button className="
                     right-10 bg-blue-400 p-2 px-3 text-white hover:bg-blue-500 cursor-pointer rounded">Send</button>
             </form>
