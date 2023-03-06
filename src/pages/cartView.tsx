b46fef2b1f206ebc292d499ca27c82df698b0249
Alex B <alex.blackburn1995@gmail.com>
2023-03-05 20:24:16 -0500

    setting up components

--- src/pages/cartView.tsx
+++ src/pages/cartView.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { HelpWidget } from '../components/HelpWidget'
+
 
 type Props = {}
 
 const cartView = (props: Props) => {
   return (
+    <main>
     <div>cartView</div>
+    <HelpWidget />
+    </main>
   )
 }
 
--- src/pages/productView.tsx
+++ src/pages/productView.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { HelpWidget } from '../components/HelpWidget'
 
 type Props = {}
 
 const productView = (props: Props) => {
   return (
+    <main>
     <div>productView</div>
+    <HelpWidget />
+    </main>
   )
 }
 
